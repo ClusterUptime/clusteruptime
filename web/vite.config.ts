af209df340c0d47f9d1e1507b54ef545d77cf1e4
Jesus Paz <jeherpabo@gmail.com>
2025-12-21 19:26:51 -0500

    feat: fix web test issues

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path"
@@ -27,6 +28,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/setup.ts"
+    setupFiles: "./src/test/setup.ts",
+    exclude: ["node_modules", "dist", "tests/*", "**/tests/**"]
   }
 });
